ba75382b90a68f322fcc92bf7afd3b3cab89e898
Michael Ge <michael.zh.ge@gmail.com>
2016-08-02 15:51:49 -0700

    weowe

--- credentials.js
+++ credentials.js
@@ -4,8 +4,9 @@ var credentials ={
 		// Replace placeholder below by the Consumer Key and Consumer Secret you got from
 		// http://developer.autodesk.com/ for the production server
 	    client_id: process.env.ClientId,
-	        client_secret: process.env.ClientSecret,
-		grant_type: 'client_credentials'
+	    client_secret: process.env.ClientSecret,
+            scopes: "data:read",
+	    grant_type: 'client_credentials'
 	},
 	// If you which to use the Autodesk View & Data API on the staging server, change this url
 	BaseUrl: 'https://developer.api.autodesk.com',
