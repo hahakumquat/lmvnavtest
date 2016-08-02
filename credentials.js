cb1dc18c60477f059a498e5694ca8a9a831649d0
Michael Ge <michael.zh.ge@gmail.com>
2016-08-02 15:54:12 -0700

    wfew

--- credentials.js
+++ credentials.js
@@ -5,7 +5,7 @@ var credentials ={
 		// http://developer.autodesk.com/ for the production server
 	    client_id: process.env.ClientId,
 	    client_secret: process.env.ClientSecret,
-            scopes: "data:read",
+            scope: "data:read",
 	    grant_type: 'client_credentials'
 	},
 	// If you which to use the Autodesk View & Data API on the staging server, change this url
